3ba396e9f8c0db45bdb43067a0fb108be4b98d2b
sreelakshmics22612 <sreelakshmics1122@gmail.com>
2026-06-08 13:10:27 +0530

    Add files via upload

---
+++ c.py
@@ -0,0 +1,7 @@
+keyword={"break","case","continue","default","defer","else","for","func","goto","if","map","range","return","struct","type","var"}
+inp=input()
+if inp in keyword:
+    print(inp+ " is keyword")
+else:
+    print(inp+ " is not keyword")
+    
